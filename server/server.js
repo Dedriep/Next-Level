ec43a473e0b1547f6ea1ced15c1af7077bc88e11
CougarChan <gohan0136@gmail.com>
2022-05-31 14:43:18 -0400

    Update

--- server/server.js
+++ server/server.js
@@ -1,22 +1,52 @@
 const express = require('express');
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+const { ApolloServer } = require('apollo-server-express');
+const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schema');
+=======
+>>>>>>> dedrie
 const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
 const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
+<<<<<<< HEAD
+=======
+>>>>>>> Ger01
+>>>>>>> dedrie
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+<<<<<<< HEAD
   context: authMiddleware,
+=======
+<<<<<<< HEAD
+
+=======
+  context: authMiddleware,
+>>>>>>> Ger01
+>>>>>>> dedrie
 });
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+ 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+=======
+>>>>>>> dedrie
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
@@ -32,6 +62,10 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 
+<<<<<<< HEAD
+=======
+>>>>>>> Ger01
+>>>>>>> dedrie
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
@@ -40,5 +74,12 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
   };
   
+<<<<<<< HEAD
   // Call the async function to start the server
+=======
+<<<<<<< HEAD
+=======
+  // Call the async function to start the server
+>>>>>>> Ger01
+>>>>>>> dedrie
   startApolloServer(typeDefs, resolvers);
