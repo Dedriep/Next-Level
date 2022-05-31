db61d1fbfee731c621b32efacfb3db287e68f406
CougarChan <gohan0136@gmail.com>
2022-05-31 19:34:10 -0400

    update

--- server/models/index.js
+++ server/models/index.js
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
 const User = require('./User')
 const Exercises = require('./Exercises')
 
 
 module.exports = {User, Exercises}
-=======
->>>>>>> dedrie
+
 const User = require('./User');
 const Thought = require('./Thought');
 
 module.exports = { User, Thought };
-<<<<<<< HEAD
-=======
->>>>>>> Ger01
->>>>>>> dedrie
--- server/server.js
+++ server/server.js
@@ -1,52 +1,33 @@
 const express = require('express');
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schema');
-=======
->>>>>>> dedrie
+
 const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
 const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
-<<<<<<< HEAD
-=======
->>>>>>> Ger01
->>>>>>> dedrie
+
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-<<<<<<< HEAD
-  context: authMiddleware,
-=======
-<<<<<<< HEAD
 
-=======
   context: authMiddleware,
->>>>>>> Ger01
->>>>>>> dedrie
+  context: authMiddleware,
+
 });
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
  
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-  
-=======
->>>>>>> dedrie
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
@@ -62,10 +43,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 
-<<<<<<< HEAD
-=======
->>>>>>> Ger01
->>>>>>> dedrie
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
@@ -74,12 +52,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
   };
   
-<<<<<<< HEAD
   // Call the async function to start the server
-=======
-<<<<<<< HEAD
-=======
-  // Call the async function to start the server
->>>>>>> Ger01
->>>>>>> dedrie
+
   startApolloServer(typeDefs, resolvers);
