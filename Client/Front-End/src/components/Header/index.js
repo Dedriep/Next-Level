46505604d7002d94a3a4364c6de3465155ce85ea
CougarChan <gohan0136@gmail.com>
2022-06-02 19:37:22 -0400

    update

--- Client/Front-End/src/components/Header/index.js
+++ Client/Front-End/src/components/Header/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1>Deep Thoughts</h1>
+        <h1>Next-Level</h1>
       </div>
     </header>
   );
