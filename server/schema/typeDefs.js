9eeba714f7a994331cf22f8facd6f628cab45dca
CougarChan <gohan0136@gmail.com>
2022-06-02 14:20:24 -0400

    update

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,30 +1,25 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-
-type User {
+  type User {
     _id: ID
     username: String
     email: String
-    
   }
 
   type Exercises {
     _id: ID
-
   }
 
-
   type Query {
-    
     user(username: String!): User
-
-
-
     users: [User]
     exercises: [Exercises]
   }
 
-
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 module.exports = typeDefs;
